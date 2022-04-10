87ae71bac9b02591d40b41d58c54d656ee1bec03
vicky lu <vicky.lu@IC-c02fq6m5md6r.attlocal.net>
2022-04-10 19:38:33 -0400

    added a few components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+
+function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("Clicked!!!");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{props.name}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
---
+++ src/components/Expenses.js
@@ -0,0 +1,31 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        date={props.items[0].date}
+        name={props.items[0].title}
+        amount={props.items[0].amount}
+      />
+      <ExpenseItem
+        date={props.items[1].date}
+        name={props.items[1].title}
+        amount={props.items[1].amount}
+      />
+      <ExpenseItem
+        date={props.items[2].date}
+        name={props.items[2].title}
+        amount={props.items[2].amount}
+      />
+      <ExpenseItem
+        date={props.items[3].date}
+        name={props.items[3].title}
+        amount={props.items[3].amount}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
